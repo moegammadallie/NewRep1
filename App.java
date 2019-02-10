dcf8fd0a1b172637fa2fbdbe93532131871c34cb
moegammadallie <47336541+moegammadallie@users.noreply.github.com>
2019-02-10 22:35:54 +0200

    Add files via upload

---
+++ App.java
@@ -0,0 +1,13 @@
+package za.ac.cput.projects;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
