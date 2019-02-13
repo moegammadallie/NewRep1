29b180af2b4ff2bd39eebb5058b4216c939096e7
moegammadallie <moegammadallie89@gmail.com>
2019-02-13 23:24:42 +0200

    Update App.java

--- App.java
+++ App.java
@@ -1,13 +1,29 @@
 package za.ac.cput.projects;
 
+import java.util.Scanner;
+
 /**
  * Hello world!
  *
  */
-public class App 
+public class App
 {
+    static Scanner console = new Scanner(System.in);
     public static void main( String[] args )
+
     {
-        System.out.println( "Hello World!" );
+        int radius;
+        double area;
+
+        //User input
+        System.out.print( "Please enter radius: " );
+        String input = console.next();
+        radius = Integer.parseInt(input);
+
+        //processing
+        area = Math.PI * Math.pow(radius, 2);
+
+        //output
+        System.out.println("The area is: " + area);
     }
 }
